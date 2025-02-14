c0d4086ff9bbb4f4a70c2db0f6dd33e1e0c7b4f8
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-14 14:38:30 -0500

    Add hooks for devenv

--- devenv.nix
+++ devenv.nix
@@ -5,11 +5,6 @@
   inputs,
   ...
 }: {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  env.API_URL = "http://localhost:5150";
-
   # https://devenv.sh/packages/
   packages = with pkgs; [
     git
@@ -17,43 +12,17 @@
     cargo-watch
     cargo-edit
     cargo-machete
-    pnpm
-    nodePackages.prettier
-    nodejs
     openssl.dev
     pkg-config
+    rustfmt
+    clippy
   ];
 
   languages.rust.enable = true;
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  processes.cargo-watch.exec = "cargo-watch -- cargo loco start";
-
-  processes.pnpm-dev.exec = "cd frontend && pnpm dev";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/tasks/
-  scripts = {
-    "start-frontend".exec = "cd frontend && pnpm dev";
-    "start-backend".exec = "cargo loco start --server-and-worker";
-
-    "build-frontend".exec = "cd frontend && pnpm build";
-    "build-backend".exec = "cargo loco build";
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+    clippy.settings.allFeatures = true;
   };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
